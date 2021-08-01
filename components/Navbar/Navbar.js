f220a623178e714dfc44e4e8a2e43973f83c19c3
Liberontissauri <jp.liberon@gmail.com>
2021-08-02 00:59:30 +0100

    added navbar

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from "./Navbar.module.css"
+import Link from "next/link"
+
+function Navbar() {
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.navbarContent}>
+                <Link href="/account"><a className={styles.navbarLink}>Account</a></Link>
+                <Link href="/account/code"><a className={styles.navbarLink}>My Code</a></Link>
+                <Link href="/"><a className={styles.mainLink}>Short Code</a></Link>
+                <Link href="/code/create"><a className={styles.navbarLink}>Create Post</a></Link>
+                <Link href="/about"><a className={styles.navbarLink}>About</a></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar/Navbar'
 import ShortHero from '../components/ShortHero/ShortHero'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Navbar></Navbar>
       <ShortHero></ShortHero>
     </div>
   )
