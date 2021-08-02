6143c61fa08f50257b27ca1c423b475c3362cb9a
Liberontissauri <jp.liberon@gmail.com>
2021-08-02 16:03:38 +0100

    added button to hero

--- components/ShortHero/ShortHero.js
+++ components/ShortHero/ShortHero.js
@@ -8,6 +8,7 @@ function ShortHero() {
         <div className={styles.background}>
             <h1 className={styles.title}>Short Code</h1>
             <h3 className={styles.subTitle}>Share code quickly with anyone</h3>
+            <button className={styles.Btn}>Start Sharing</button>
             <img className={styles.waves} alt="wave" src={waves.src}/>
         </div>
     )
