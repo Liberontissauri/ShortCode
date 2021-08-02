9d93cc8efe7caf4ef83028618c092a80dafa577f
Liberontissauri <jp.liberon@gmail.com>
2021-08-02 15:24:36 +0100

    added title and subtitle to hero

--- components/ShortHero/ShortHero.js
+++ components/ShortHero/ShortHero.js
@@ -6,6 +6,8 @@ import waves from "../../images/waves.png"
 function ShortHero() {
     return (
         <div className={styles.background}>
+            <h1 className={styles.title}>Short Code</h1>
+            <h3 className={styles.subTitle}>Share code quickly with anyone</h3>
             <img className={styles.waves} alt="wave" src={waves.src}/>
         </div>
     )
