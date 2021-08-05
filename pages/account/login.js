052a9e56922e359c3d50c42a746a871cd9c22ed8
Liberontissauri <jp.liberon@gmail.com>
2021-08-05 18:59:04 +0100

    added login page

---
+++ pages/account/login.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+
+import styles from "../../styles/Login.module.css"
+
+import NavBar from "../../components/Navbar/Navbar"
+import {toast, Toaster} from 'react-hot-toast'
+import axios from 'axios'
+
+export default function code_id() {
+    const router = useRouter()
+    const [keyValue, setKeyValue] = useState("")
+
+    //There is probably a lot of better solutions to this, so it should be changed later
+    const api_server_url = "http://localhost:5000"
+    //
+
+    return (
+        <div className={styles.background}>
+            <Toaster
+            toastOptions={{
+                className:"",
+                style: {
+                    color:"#fff",
+                    fontFamily:"'Hind Vadodara', sans-serif",
+                    backgroundColor: "#3d3d3d",
+                }
+            }}
+            />
+            <NavBar></NavBar>
+            <div className={styles.formDiv}>
+                <h2 className={styles.title}>Login</h2>
+                <h3 className={styles.subTitle}>Or Sign up</h3>
+            </div>
+            <div className={styles.formDiv}>
+                <input placeholder="API key" onInput={e => setKeyValue(e.target.value)}></input>
+            </div>
+            <div className={styles.formDiv}>
+                <button className={styles.submitButton} onClick={initLogin}>Login</button>
+            </div>
+        </div>
+    )
+    function handleError(err) {
+        if(err.response.data.type == "InvalidInput") toast.error(err.response.data.message)
+
+        return false
+    }
+    async function initLogin() {
+        const key = await axios.get(`${api_server_url}/apikeys/${keyValue}`)
+                        .catch(err => handleError(err))
+        if(!key) return
+
+        const storage = window.localStorage
+        storage.setItem("api_key", key.data.key_id)
+        storage.setItem("email", key.data.email)
+        
+        toast.success("Logged in Successfully!")
+    }
+}
