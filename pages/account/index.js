02acd9281350da3138fb13482191566c09cd9f02
Liberontissauri <jp.liberon@gmail.com>
2021-08-07 18:25:04 +0100

    added login page ui

---
+++ pages/account/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+import styles from "../../styles/Account.module.css"
+
+import Navbar from '../../components/Navbar/Navbar'
+
+export default function Account() {
+    const api_key = useState("kmklkGMGMSdgdsmg,elwml")
+    const email = useState("someone.else@gmail.com")
+
+    return (
+        <div className={styles.background}>
+            <Navbar></Navbar>
+            <h1 className={styles.title}>Account</h1>
+            <div className={styles.contentDiv}>
+                <h2 className={styles.subTitle}>General Account Information</h2>
+                <ul className={styles.list}>
+                    <li><p>API key:</p><p>{api_key}</p></li>
+                    <li><p>Email:</p><p>{email}</p></li>
+                </ul>
+                <h2 className={styles.subTitle} style={{color: "#C23535"}}>Dangerous Zone</h2>
+                <button className={styles.dangerBtn}>Logout</button>
+                <button className={styles.dangerBtn}>DELETE ACCOUNT</button>
+            </div>
+        </div>
+    )
+}
