2b0f7a184df2fa3a20d748fe4605153ca4294b88
Liberontissauri <jp.liberon@gmail.com>
2021-08-10 15:29:03 +0100

    added Code Input component

---
+++ components/CodeInput/CodeInput.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import {React, useEffect, useState} from 'react'
+import router from 'next/router'
+
+import styles from "./CodeInput.module.css"
+
+function CodeContainer(props) {
+    const [Title, setTitle] = useState("")
+    const [Language, setLanguage] = useState("")
+    const [Code, setCode] = useState("")
+
+    useEffect(()=>{
+        const storage = window.localStorage
+        if(!storage.getItem("email") || !storage.getItem("api_key")) {
+            router.push("/account/login")
+        }
+    })
+
+    return (
+        <div>
+            <input className={styles.inputSmall} placeholder="Title" onChange={(e) => setTitle(e.target.value)}></input>
+            <input className={styles.inputSmall} placeholder="Language" onChange={(e) => setLanguage(e.target.value)}></input>
+            <textarea placeholder="Code" type="text" className={styles.container} spellCheck="false"  onChange={(e) => setCode(e.target.value)}>
+                
+            </textarea>
+            <button className={styles.Btn} onClick={createPost}>Submit Post</button>
+        </div>
+    )
+    async function createPost() {
+        const storage = window.localStorage
+        const SERVER_URL = "http://localhost:5000"
+        const res = await axios.post(`${SERVER_URL}/codes`, {
+            keyId: storage.getItem("api_key"),
+            title: Title,
+            code: Code,
+            language: Language
+        })
+    }
+}
+
+export default CodeContainer
