2d113d9ad348401ca99ad126499e0ad590746b64
Liberontissauri <jp.liberon@gmail.com>
2021-08-17 19:42:12 +0100

    fixed create post link on navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
                 <Link href="/account"><a className={styles.navbarLink}>Account</a></Link>
                 <Link href="/account/code"><a className={styles.navbarLink}>My Code</a></Link>
                 <Link href="/"><a className={styles.mainLink}>Short Code</a></Link>
-                <Link href="/code/create"><a className={styles.navbarLink}>Create Post</a></Link>
+                <Link href="/code/new"><a className={styles.navbarLink}>Create Post</a></Link>
                 <Link href="/about"><a className={styles.navbarLink}>About</a></Link>
             </div>
         </div>
