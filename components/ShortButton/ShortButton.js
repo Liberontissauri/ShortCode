594a5e36e3dc23ab07df329577d98749e05b8db7
Liberontissauri <jp.liberon@gmail.com>
2021-08-18 15:24:22 +0100

    added ShortButton component

---
+++ components/ShortButton/ShortButton.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from "./ShortButton.module.css"
+
+function ShortButton(props) {
+    function getSizeStyle(styles, size) {
+        switch (size) {
+            case "big":
+                return styles.bigSize;
+            case "small":
+                return styles.smallSize;
+            default:
+                return styles.normalSize;
+        }
+    }
+    function getTypeStyle(styles, type) {
+        switch (type) {
+            case "danger":
+                return styles.dangerType;
+            default:
+                return styles.normalType;
+        }
+    }
+    
+    return (
+        <div>
+            <button className={`${styles.buttonBase} ${getSizeStyle(styles, props.size)} ${getTypeStyle(styles, props.type)}`}>{props.text}</button>
+        </div>
+    )
+}
+
+export default ShortButton
