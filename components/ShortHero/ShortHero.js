72c2b0cfa9587399f929757fc376140594749fdd
Liberontissauri <jp.liberon@gmail.com>
2021-08-18 15:25:01 +0100

    replaced normal html button elements with the ShortButton component on the main page

--- components/ShortHero/ShortHero.js
+++ components/ShortHero/ShortHero.js
@@ -2,13 +2,14 @@ import React from 'react'
 import styles from "./ShortHero.module.css"
 
 import waves from "../../images/waves.png"
+import ShortButton from '../ShortButton/ShortButton'
 
 function ShortHero() {
     return (
         <div className={styles.background}>
             <h1 className={styles.title}>Short Code</h1>
             <h3 className={styles.subTitle}>Share code quickly with anyone</h3>
-            <button className={styles.Btn}>Start Sharing</button>
+            <ShortButton text="Start Sharing" size="big" type="normal"></ShortButton>
             <img className={styles.waves} alt="wave" src={waves.src}/>
         </div>
     )
